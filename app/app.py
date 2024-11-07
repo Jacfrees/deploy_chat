55b44b663bf288996d0c4cf215c3c8c7f9d435f4
Jacfrees <jimmyalarcon0@gmail.com>
2024-11-07 17:27:21 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -66,5 +66,4 @@ def ask_question():
     return jsonify({'Question': question, 'IA Response': bedrock_response})
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+app = Flask(__name__)
